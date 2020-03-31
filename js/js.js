a5f10f6b1b19ee925ac3414680d239281e6d1f1e
dcervantesr <dcervantesr@gmail.com>
2020-03-30 21:19:00 -0300

    mostrando la bienvenida al usuario

--- js/js.js
+++ js/js.js
@@ -3,5 +3,9 @@ $(document).ready(function() {
         var valor = $("#usuario")[0].value;
         $("#autenticacion").hide();
         $("#registro").hide();
+        let bienvenida = `<div class="p-2" id="bienvenida">
+                            <span>Bienvenido ${valor}</span>
+                          </div>`;
+        $("#topBar")[0].innerHTML += bienvenida;
     });
 });
